a1d116968089e796772fd30c068f2ef4301b7cc1
Samuel Barbosa <samu101108@gmail.com>
2022-05-08 10:49:43 -0300

    feat: Adicionada section Novidades Define

--- homepage.php
+++ homepage.php
@@ -5,6 +5,14 @@
 
 $context = Timber::context();
 
+// Contexto e Query dos blog posts
+$args = array(
+   'post_type' => 'post',
+   'posts_per_page' => 2,
+);
+$context['posts'] = new Timber\PostQuery($args);
+//End - Contexto e Query dos blog posts
+
 $timber_post     = new Timber\Post();
 $context['post'] = $timber_post;
 Timber::render( array( 'page-' . $timber_post->post_name . '.twig', 'homepage.twig' ), $context );
