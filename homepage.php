74d08a880dfbb75b695a851e2502f2991fc4e3d8
Samuel Barbosa <samu101108@gmail.com>
2022-05-08 15:59:55 -0300

    feat: Iniciando a configuração de depoimentos

--- homepage.php
+++ homepage.php
@@ -13,6 +13,16 @@ $args = array(
 $context['posts'] = new Timber\PostQuery($args);
 //End - Contexto e Query dos blog posts
 
+// Contexto e Query dos posts depoimentos
+$args = array(
+   'post_type' => 'depoimento',
+   'posts_per_page' => -1,
+);
+$context['depoimentos'] = new Timber\PostQuery($args);
+//End - Contexto e Query dos  posts depoimentos
+
+
 $timber_post     = new Timber\Post();
 $context['post'] = $timber_post;
+$context['depoimento'] = $timber_post;
 Timber::render( array( 'page-' . $timber_post->post_name . '.twig', 'homepage.twig' ), $context );
