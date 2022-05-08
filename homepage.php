286df1103d5166fbc67ca2694603915508981b8e
Samuel Barbosa <samu101108@gmail.com>
2022-05-08 19:24:53 -0300

    feat: Iniciando a configuração das Marcas parceiras

--- homepage.php
+++ homepage.php
@@ -21,6 +21,14 @@ $args = array(
 $context['depoimentos'] = new Timber\PostQuery($args);
 //End - Contexto e Query dos  posts depoimentos
 
+// Contexto e Query dos posts marca
+$args = array(
+   'post_type' => 'marca',
+   'posts_per_page' => -1,
+);
+$context['marcas'] = new Timber\PostQuery($args);
+//End - Contexto e Query dos  posts marcas
+
 
 $timber_post     = new Timber\Post();
 $context['post'] = $timber_post;
