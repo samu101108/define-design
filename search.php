38505ad58ba2dce3de29c04594af542fa3369e8e
Wanderson Silva <wandersonsilva768@gmail.com>
2022-05-31 23:58:44 -0300

    feat: formatação inicial da área de busca do site

--- search.php
+++ search.php
@@ -9,10 +9,10 @@
  * @since   Timber 0.1
  */
 
-$templates = array( 'search.twig', 'archive.twig', 'index.twig' );
+$templates = array( 'search.twig' );
 
 $context          = Timber::context();
-$context['title'] = 'Search results for ' . get_search_query();
+$context['title'] = 'Exibindo resultados para ' . get_search_query();
 $context['posts'] = new Timber\PostQuery();
 
 Timber::render( $templates, $context );
